92fe4292ef75c0147ceb878df2c92d7d1388f7f8
MdTashkinurAhammedTonmo <md11tonmoy1613@gmail.com>
2025-12-19 00:39:23 +0600

    card Delete Complete

--- index.js
+++ index.js
@@ -1,129 +1,88 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const port = 3000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// // const uri = "mongodb+srv://food-lover-server:7LX6eEoglDBmqbDB@bd-developer.9vf4bfe.mongodb.net/?appName=BD-Developer";
-// const uri =
-//   "mongodb+srv://tonmoydb:umGAebda3yrUCUfl@bd-developer.9vf4bfe.mongodb.net/?appName=BD-Developer";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     //  await client.connect();
-
-//     const db = client.db("food-db");
-//     const modelCollection = db.collection("product");
-
-//     app.get("/product", async (req, res) => {
-//       const result = await modelCollection.find().toArray();
-//       console.log(result);
-
-//       res.send({});
-//     });
-
-//     // await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("server is runing fine");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("ami valo nei");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://food-lover-server:7LX6eEoglDBmqbDB@bd-developer.9vf4bfe.mongodb.net/?appName=BD-Developer";
 
-const uri = "mongodb+srv://food-lover-server:7LX6eEoglDBmqbDB@bd-developer.9vf4bfe.mongodb.net/?appName=BD-Developer";
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const client = new MongoClient(uri);
 
 async function run() {
   try {
     await client.connect();
-
     const db = client.db("fooddb");
     const modelCollection = db.collection("foods");
 
+    // ✅ GET all foods
     app.get("/foods", async (req, res) => {
       const result = await modelCollection.find().toArray();
-      
       res.send(result);
     });
 
-    app.get('/foods/:id',async(req,res) => {
-      const {id} = req.params
-      console.log(id)
-      const objectId = new ObjectId(id)
+    // ✅ GET single food
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await modelCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send({ success: true, result });
+    });
 
-      const result = await modelCollection.findOne({_id: objectId})
+    // ✅ POST food
+    app.post("/foods", async (req, res) => {
+      const result = await modelCollection.insertOne(req.body);
+      res.send({ success: true, result });
+    });
 
-      res.send({
-        success:true,
-        result
-      })
-    })
+    // ✅ UPDATE food
+    app.put("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
 
+      const result = await modelCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: data }
+      );
 
-    app.post('/foods', async (req,res) => {
-      const data = req.body
-      console.log(data)
-      const result = await modelCollection.insertOne(data)
       res.send({
         success: true,
-        result
-      })
-    })
+        modifiedCount: result.modifiedCount,
+      });
+    });
 
-    console.log("MongoDB connection established.");
+    // ✅ DELETE food (THIS WAS BROKEN BEFORE)
+    app.delete("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await modelCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send({
+        success: true,
+        deletedCount: result.deletedCount,
+      });
+    });
+
+    console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error("MongoDB error:", err);
+    console.log(err);
   }
 }
 
 run();
 
 app.get("/", (req, res) => {
-  res.send("server is running fine");
+  res.send("Server is running");
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
